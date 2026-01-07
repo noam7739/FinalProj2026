6c749594d3338e22bbe9cf813eefcb43433962d0
eyal <eyalbendavid2008@gmail.com>
2026-01-07 14:56:10 +0200

    add space line

--- app/src/main/java/com/example/noamfinalproj/MainActivity.java
+++ app/src/main/java/com/example/noamfinalproj/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+
         btnLocker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
