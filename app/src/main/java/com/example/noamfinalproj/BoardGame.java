00ceee59dd23f69f8db0f1dca7eace6660e473bf
noam <bozaglonoam77@gmail.com>
2026-05-13 13:52:09 +0300

    simple java

--- app/src/main/java/com/example/noamfinalproj/BoardGame.java
+++ app/src/main/java/com/example/noamfinalproj/BoardGame.java
@@ -14,6 +14,7 @@ import java.util.Random;
 
 public class BoardGame extends View {
     private boolean isRunning = true;
+
     private Paint scorePaint, infoPaint, winPaint, goalAnnouncePaint, buttonPaint, buttonTextPaint, uiBoxPaint, linePaint;
     private Drawable backgroundDrawable, goalDrawable;
     private Player player;
